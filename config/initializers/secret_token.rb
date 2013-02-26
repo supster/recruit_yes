ede38f7bd9a1df9ff2c125df3c9a92739bf819a5
Ek Indharameesup <eindhara@Eks-MacBook-Air.local>
2013-02-26 15:27:25 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecruitYes::Application.config.secret_token = 'dbdb168a3362afa22c1945f3ca944a10f569227f5cc3693fd5fe3642e96bf466ec1c0be48bcfd18b779da6d1903d867afe4f2534d4f536284f67c25eff9c1ea8'
