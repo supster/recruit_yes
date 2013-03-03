bf472300e2fb18408cd11c031efbd16c3592330a
supster <supatra.indhara@gmail.com>
2013-03-03 16:03:10 -0500

    sign-in-out

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Authentication" do
+  
+  subject { page }
+  
+  describe "signin page" do
+    before { visit signin_path }
+  
+    it { should have_selector('h1',     text: 'Sign In') }
+    it { should have_selector('title',  text: full_title('Sign In'))}
+
+  end
+  
+  describe "signin" do
+    before { visit signin_path }
+    
+      describe "with invalid information" do
+        before { click_button "Sign in" }
+      end
+  end
+end
